fd37a0af6cc9336594e2158ebb0846313ba2ec90
b0gavmar <b0gavmar@vasvari.org>
2025-03-08 15:29:58 +0100

    uj projekt

--- 2feladat/SzamnegyesProject/SzamnegyesProject/ViewModels/MainWindowViewModel.cs
+++ 2feladat/SzamnegyesProject/SzamnegyesProject/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SzamnegyesProject.Models;
 
 namespace SzamnegyesProject.ViewModels
 {
-    public class MainWindowViewModel
+    public partial class MainWindowViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private Szamnegyes a;
+        [ObservableProperty]
+        private Szamnegyes b;
+        [ObservableProperty]
+        private Szamnegyes c;
+        [ObservableProperty]
+        private Szamnegyes d;
 
+        public MainWindowViewModel()
+        {
+            A = new Szamnegyes();
+            B = new Szamnegyes();
+            C = new Szamnegyes();
+            D = new Szamnegyes();
+        }
+
+        [RelayCommand]
+        public void IncreaseTableA()
+        {
+            A.Increase(1);
+        }
+
+        [RelayCommand]
+        public void IncreaseTableB()
+        {
+            B.Increase(2);
+        }
+
+        [RelayCommand]
+        public void IncreaseTableC()
+        {
+            C.Increase(3);
+        }
+
+        [RelayCommand]
+        public void IncreaseTableD()
+        {
+            D.Increase(4);
+        }
     }
 }
---
+++ Szamnegyes.Shared/Szamnegyes.Shared/Models/Szamnegyes.cs
@@ -0,0 +1,26 @@
+﻿namespace SzamnegyesProject.Models
+{
+    public class Szamnegyes
+    {
+        public Guid id { get; set; } = Guid.NewGuid();
+        public int[] fours = new int[4];
+
+        public Szamnegyes()
+        {
+            
+        }
+
+        public void Increase(int tableNumber)
+        {
+            for(int i = 0; i<4;i++)
+            {
+                fours[i]++;
+            }
+        }
+
+        public void NullTable()
+        {
+            fours = new int[4];
+        }
+    }
+}
